f6a02316e0ede41cbcd64d184284ff6127a5c9c6
hilal görgülüarslan <101900441+gorguluarslan@users.noreply.github.com>
2022-12-25 20:19:23 +0300

    update: add contextprovider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import ChangeTheme from "./components/ChangeTheme";
-import ThemeContext from "./contexts/ThemeContext";
+import { ThemeContextProvider } from "./contexts/ThemeContext";
 
 function App() {
   return (
     <div>
-      <ThemeContext.Provider value="light">
+      <ThemeContextProvider>
         <ChangeTheme />
-      </ThemeContext.Provider>
+      </ThemeContextProvider>
     </div>
   );
 }
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -2,4 +2,8 @@ import { createContext } from "react";
 
 const ThemeContext = createContext();
 
+export const ThemeContextProvider = ({ children }) => {
+  return <ThemeContextProvider value="light">{children}</ThemeContextProvider>;
+};
+
 export default ThemeContext;
